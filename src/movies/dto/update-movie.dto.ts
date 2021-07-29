37afbe4685425ba3a683ad4fa053f3f136618512
JeUpark <genhcode99@gmail.com>
2021-07-29 09:59:22 +0900

    #2.6 Modules and Dependency Injection

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDto } from './create-movie.dto';
-import { IsNumber, IsString } from 'class-validator';
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
