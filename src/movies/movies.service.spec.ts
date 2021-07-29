9e6a712baef887e571267d798ffdc45f7d42a02c
JeUpark <genhcode99@gmail.com>
2021-07-29 11:36:41 +0900

    #3.1 Your first Unit Test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -15,4 +15,8 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('shoud be 4', () => {
+    expect(2 + 3).toEqual(5);
+  });
 });
